edff34652d3839533b0092307f3ddbbe2a90e572
raghvendra dharmapurikar <raghvendra.d@gmail.com>
2011-04-28 07:51:46 +0530

    second commit

---
+++ grails-app/controllers/org/grails/twitter/StatusController.groovy
@@ -0,0 +1,59 @@
+package org.grails.twitter
+
+import grails.plugins.springsecurity.Secured
+
+import org.grails.twitter.auth.Person
+
+@Secured('IS_AUTHENTICATED_FULLY')
+class StatusController {
+
+	def springSecurityService
+	def statusService
+	
+    def index = {				
+		def messages = currentUserTimeline()
+		[statusMessages: messages]
+    }
+	
+	def updateStatus = {		
+		def status = new Status(message: params.message)
+		status.author = lookupPerson()
+		status.save()		
+        def messages = currentUserTimeline()
+        render template: 'statusMessages', collection: messages, var: 'statusMessage'
+	}
+
+	def follow = {
+		def per = Person.get(params.id)
+		if(per) {
+			def currentUser = lookupPerson()
+			currentUser.addToFollowed(per)
+			currentUser.save()			
+		}
+		redirect action: 'index'
+	}
+	
+	private lookupPerson() {
+		Person.get(springSecurityService.principal.id)
+	}
+		
+	private currentUserTimeline() {		
+
+		   def per = lookupPerson()
+		   
+		   def messages = Status.withCriteria {
+				or {
+					author {
+						eq 'username', per.username
+					}
+					if (per.followed) {
+						inList 'author', per.followed
+					}
+				}
+				maxResults 10
+				order 'dateCreated', 'desc'
+			}
+		messages
+	}
+	
+}
---
+++ grails-app/domain/org/grails/twitter/Status.groovy
@@ -0,0 +1,13 @@
+package org.grails.twitter
+
+import org.grails.twitter.auth.*
+
+class Status {
+
+	String message
+	Person author
+	Date dateCreated
+	
+    static constraints = {
+    }
+}
